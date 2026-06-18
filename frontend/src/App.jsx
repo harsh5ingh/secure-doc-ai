3f09d9b926bce4875599734074b7f9ffdd8e3276
Harsh Kumar <95859326+harsh5ingh@users.noreply.github.com>
2026-06-18 16:41:16 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
-import Login from "./pages/login.jsx"
+import Login from "./pages/Login.jsx"
 import Signup from "./pages/Signup.jsx"
 import Dashboard from "./pages/Dashboard.jsx"
 import ProtectedRoute from "./components/ProtectedRoute.jsx"
